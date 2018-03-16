404ea59200f8078a35e688c9d174c6506cdb575f
fox <414802577@qq.com>
2018-03-16 10:48:02 +0800

    添加断言

--- src/main/java/modle/Login.java
+++ src/main/java/modle/Login.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.pagefactory.ElementLocator;
 import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+import org.testng.Assert;
 
 import java.lang.reflect.Field;
 
@@ -57,6 +58,6 @@ public class Login {
     }
 
     public void check(){
-
+        Assert.assertEquals(loginResult.isDisplayed(),true);
     }
 }
